6cac8bbd943581b0a0edbf123a03d0f87f4d2855
teddy <teddytw1209@gmail.com>
2024-10-26 14:40:31 -0400

    update

--- main_finetune.py
+++ main_finetune.py
@@ -24,7 +24,8 @@ from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 
 import util.lr_decay as lrd
 import util.misc as misc
-from util.datasets import build_dataset, DistributedSamplerWrapper
+from util.datasets import build_dataset
+from catalyst.data import DistributedSamplerWrapper
 from util.pos_embed import interpolate_pos_embed
 from util.misc import NativeScalerWithGradNormCount as NativeScaler
 
--- util/datasets.py
+++ util/datasets.py
@@ -4,12 +4,7 @@
 # --------------------------------------------------------
 
 import os
-from typing import Iterator, List, Optional, Union
-from operator import itemgetter
 
-from torch.utils.data import DistributedSampler
-
-from catalyst.data.dataset import DatasetFromSampler
 from torchvision import datasets, transforms
 from timm.data import create_transform
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
@@ -58,56 +53,3 @@ def build_transform(is_train, args):
     t.append(transforms.ToTensor())
     t.append(transforms.Normalize(mean, std))
     return transforms.Compose(t)
-
-## sampler wrapper
-class DistributedSamplerWrapper(DistributedSampler):
-    """
-    Wrapper over `Sampler` for distributed training.
-    Allows you to use any sampler in distributed mode.
-
-    It is especially useful in conjunction with
-    `torch.nn.parallel.DistributedDataParallel`. In such case, each
-    process can pass a DistributedSamplerWrapper instance as a DataLoader
-    sampler, and load a subset of subsampled data of the original dataset
-    that is exclusive to it.
-
-    .. note::
-        Sampler is assumed to be of constant size.
-    """
-
-    def __init__(
-        self,
-        sampler,
-        num_replicas: Optional[int] = None,
-        rank: Optional[int] = None,
-        shuffle: bool = True,
-    ):
-        """
-
-        Args:
-            sampler: Sampler used for subsampling
-            num_replicas (int, optional): Number of processes participating in
-                distributed training
-            rank (int, optional): Rank of the current process
-                within ``num_replicas``
-            shuffle (bool, optional): If true (default),
-                sampler will shuffle the indices
-        """
-        super(DistributedSamplerWrapper, self).__init__(
-            DatasetFromSampler(sampler),
-            num_replicas=num_replicas,
-            rank=rank,
-            shuffle=shuffle,
-        )
-        self.sampler = sampler
-
-    def __iter__(self) -> Iterator[int]:
-        """Iterate over sampler.
-
-        Returns:
-            python iterator
-        """
-        self.dataset = DatasetFromSampler(self.sampler)
-        indexes_of_indexes = super().__iter__()
-        subsampler_indexes = self.dataset
-        return iter(itemgetter(*indexes_of_indexes)(subsampler_indexes))
