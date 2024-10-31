d6e90e5c9e6867deadf99f8482936d1bc630648d
teddy <teddytw1209@gmail.com>
2024-10-31 15:04:29 -0400

    Update finetune_retfound_study.sh

--- finetune_retfound_study.sh
+++ finetune_retfound_study.sh
@@ -45,5 +45,5 @@ fi
 # Modify the path to your singularity container 
 
 python -m torch.distributed.launch --nproc_per_node=1 --master_port=48798 main_finetune.py     --batch_size 16     --world_size 1     --model vit_large_patch16     --epochs 50 \
-    --blr 5e-3 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path ./dataset/ADMCI_study/all_lists_data-$SUBSTUDY.csv     --task $STUDY-$SUBSTUDY-$BALSAMPLER \
+    --blr 5e-3 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path ./dataset/ADMCI_study/all_lists_data-$SUBSTUDY.csv     --task $STUDY-$SUBSTUDY-$BALSAMPLER/ \
     --finetune ./pretrain_OCT/RETFound_oct_weights.pth     --input_size 224 $ADDCMD
