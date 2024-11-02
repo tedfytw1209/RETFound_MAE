e59487bba3c468bf8449786ca0ddb48953681b10
teddy <teddytw1209@gmail.com>
2024-11-01 22:34:45 -0400

    Update multiple_run.sh

--- multiple_run.sh
+++ multiple_run.sh
@@ -14,10 +14,10 @@ do
     for PROXIMAL in "${PROXIMALS[@]}"
     do
       # Create a job name based on the variables
-      JOB_NAME="study${DIVIDE}${MODEL_NAME}${NUM_CLASS}${PROXIMAL}"
+      JOB_NAME="study3${DIVIDE}${MODEL_NAME}${NUM_CLASS}${PROXIMAL}"
       echo $JOB_NAME
       # Submit the job to Slurm
-      sbatch --job-name="$JOB_NAME" finetune_retfound_study.sh study2 $MODEL_NAME $NUM_CLASS $PROXIMAL $DIVIDE
+      sbatch --job-name="$JOB_NAME" finetune_retfound_study.sh study3 $MODEL_NAME $NUM_CLASS $PROXIMAL $DIVIDE
     done
   done
 done
