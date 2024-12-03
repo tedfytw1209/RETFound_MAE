533b695481b1706a7a9f198982380814d9b81fab
teddy <teddytw1209@gmail.com>
2024-12-02 21:39:02 -0500

    Update finetune_retfound_multiimg_finetune.sh

--- finetune_retfound_multiimg_finetune.sh
+++ finetune_retfound_multiimg_finetune.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=32gb
 #SBATCH --partition=gpu
-#SBATCH --gpus=a100:8
-#SBATCH --time=48:00:00
+#SBATCH --gpus=a100:1
+#SBATCH --time=70:00:00
 #SBATCH --output=%x.%j.out
 
 
@@ -29,6 +29,6 @@ echo $Num_CLASS
 
 # Modify the path to your singularity container 
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=$MASTER_PORT main_finetune.py     --batch_size 16     --world_size 1     --model vit_large_patch16     --epochs 50 \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune.py     --batch_size 16     --world_size 1     --model vit_large_patch16     --epochs 50 \
     --blr 5e-3 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path ./dataset/ADMCI_study/${DIVIDE}_lists_data-$SUBSTUDY.csv     --task $STUDY-$DIVIDE-$SUBSTUDY-$ADDCMD-tr${NUM_K}/ \
     --finetune ./pretrain_OCT/RETFound_oct_weights.pth --input_size 224 --num_k $NUM_K --img_dir /orange/bianjiang/tienyu/OCT_AD/all_3d_oct/
