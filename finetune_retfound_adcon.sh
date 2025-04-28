4408eda03bdd15460107c2a8e666ab58b9c6fe94
teddy <teddytw1209@gmail.com>
2025-04-27 22:31:42 -0400

    new batch

---
+++ finetune_retfound_adcon.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16gb
+#SBATCH --partition=gpu
+#SBATCH --gpus=a100:1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --account=yonghui.wu
+#SBATCH --qos=yonghui.wu
+
+date;hostname;pwd
+
+module load conda
+conda activate retfound
+# Go to home directory
+#cd $HOME
+STUDY=$1
+SUBSTUDY=$2
+Num_CLASS=$3
+DIVIDE=${4:-"all"}
+NUM_K=${5:-"0"}
+ADDCMD=${6:-""}
+
+MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+
+echo $SUBSTUDY
+echo $Num_CLASS
+
+# Modify the path to your singularity container 
+
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune.py     --batch_size 16     --world_size 1     --model vit_large_patch16     --epochs 50 \
+    --blr 5e-3 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path /blue/ruogu.fang/tienyuchang/OCTAD_old/all_lists_data-ad_control_repath.csv     --task $STUDY-$DIVIDE-$SUBSTUDY-$ADDCMD/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ \
+    --finetune /orange/ruogu.fang/tienyuchang/retfound_pretrain_model/RETFound_oct_weights.pth     --input_size 224 --num_k $NUM_K $ADDCMD
