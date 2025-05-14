e3ef7874215654e94e3f7973f68f0be2755d7e04
teddy <teddytw1209@gmail.com>
2025-05-14 12:02:23 -0400

    Update finetune_retfound_benchmark.sh

--- finetune_retfound_benchmark.sh
+++ finetune_retfound_benchmark.sh
@@ -32,4 +32,4 @@ echo $Num_CLASS
 
 # Modify the path to your singularity container 
 
-torchrun --nproc_per_node=1 --master_port=48798 main_finetune.py --savemodel --global_pool    --batch_size 16     --world_size 1     --model RETFound_mae     --epochs 100 --blr 2e-5 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path $DATA_PATH     --task $STUDY-$SUBSTUDY-$ADDCMD-$ADDCMD2/ --finetune $PRETRAIN_MODEL --num_workers 8 --input_size 224 $ADDCMD $ADDCMD2 $ADDCMD3
+torchrun --nproc_per_node=1 --master_port=48798 main_finetune.py --savemodel --global_pool    --batch_size 16     --world_size 1     --model RETFound_mae     --epochs 50 --blr 5e-4 --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path $DATA_PATH     --task $STUDY-$SUBSTUDY-$ADDCMD-$ADDCMD2/ --finetune $PRETRAIN_MODEL --num_workers 8 --input_size 224 $ADDCMD $ADDCMD2 $ADDCMD3
