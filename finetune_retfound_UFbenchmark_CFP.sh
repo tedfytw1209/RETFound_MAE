da8aef6587cf98b38cfabf1e14f737bb30940c31
teddy <teddytw1209@gmail.com>
2025-06-04 23:34:57 -0400

    Update finetune_retfound_UFbenchmark_CFP.sh

--- finetune_retfound_UFbenchmark_CFP.sh
+++ finetune_retfound_UFbenchmark_CFP.sh
@@ -29,5 +29,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-
+# sbatch finetune_retfound_UFbenchmark_CFP.sh DR_all_split RETFound_mae RETFound_mae_natureCFP 5e-4 5
 torchrun --nproc_per_node=1 --master_port=48798 main_finetune.py --savemodel --global_pool    --batch_size 16     --world_size 1     --model $MODEL     --epochs 100 --lr $LR --layer_decay 0.65     --weight_decay 0.05 --drop_path 0.2     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/new/split/tune5-eval5/${STUDY}.csv     --task $STUDY-all-$MODEL-$ADDCMD-$ADDCMD2/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K $ADDCMD $ADDCMD2 --modality CFP
