09657e97b245bb4f74922050235ebbc0dbb2c3bc
teddy <teddytw1209@gmail.com>
2025-06-07 17:43:18 -0400

    Create finetune_retfound_UFbenchmark_v2.sh

---
+++ finetune_retfound_UFbenchmark_v2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --partition=gpu
+#SBATCH --gpus=a100:1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+
+date;hostname;pwd
+
+module load conda
+conda activate retfound_new
+# Go to home directory
+#cd $HOME
+STUDY=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 5, Glaucoma_all_split 5
+MODEL=${2:-"RETFound_mae"}
+FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
+LR=${4:-"1e-3"}
+Num_CLASS=${5:-"2"}
+weight_decay=${6:-"0.05"}
+NUM_K=${7:-"0"}
+ADDCMD=${8:-""}
+ADDCMD2=${9:-""}
+
+MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+
+echo $SUBSTUDY
+echo $Num_CLASS
+
+# Modify the path to your singularity container 
+# sbatch finetune_retfound_UFbenchmark.sh DR_all_split RETFound_mae RETFound_mae_natureOCT 1e-3 5
+torchrun --nproc_per_node=1 --master_port=48798 main_finetune.py --savemodel --global_pool    --batch_size 16     --world_size 1     --model $MODEL     --epochs 100 --lr $LR --layer_decay 0.65     --weight_decay $weight_decay --drop_path 0.2     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/new_v2/split/tune5-eval5/${STUDY}.csv     --task $STUDY-all-$MODEL-$ADDCMD-$ADDCMD2/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K $ADDCMD $ADDCMD2
