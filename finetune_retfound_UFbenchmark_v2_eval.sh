fd1c837a27d28774e94f3769dc4695769377ec19
teddy <teddytw1209@gmail.com>
2025-06-11 23:17:05 -0400

    Update finetune_retfound_UFbenchmark_v2_eval.sh

--- finetune_retfound_UFbenchmark_v2_eval.sh
+++ finetune_retfound_UFbenchmark_v2_eval.sh
@@ -18,7 +18,8 @@ STUDY=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 5, Glaucoma_all_sp
 MODEL=${2:-"RETFound_mae"}
 FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
 RESUME=${4:-"0"} # 0 for no resume, 1 for resume
-NUM_K=${5:-"0"}
+Num_CLASS=${5:-"2"} # 2 for AMD, 5 for DR, 5 for Glaucoma, 2 for Cataract
+NUM_K=${6:-"0"}
 
 MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 
@@ -26,5 +27,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_UFbenchmark_v2.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT 5e-4 2
+# sbatch finetune_retfound_UFbenchmark_v2_eval.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT 0 2
 TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 16     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/new_v2/split/tune5-eval5/${STUDY}.csv     --task $STUDY-all-$MODEL-XAI-EVAL/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K --resume $RESUME
