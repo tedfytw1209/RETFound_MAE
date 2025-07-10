dd7fff588c480735cc8048ad75a1435169057236
teddy <teddytw1209@gmail.com>
2025-07-10 10:11:47 -0400

    XAI script

---
+++ XAI_eval_script.sh
@@ -0,0 +1,9 @@
+
+## Attention-based XAI
+sbatch finetune_retfound_UFbenchmark_v2_eval.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT output_dir/AMD_all_split-IRB2024_v2-all-RETFound_mae-OCT-mcceval---testval-/checkpoint-best.pth 2 attn
+sbatch finetune_retfound_UFbenchmark_v2_eval.sh Cataract_all_split RETFound_mae RETFound_mae_natureOCT output_dir/Cataract_all_split-IRB2024_v2-all-RETFound_mae-OCT-mcceval---testval-/checkpoint-best.pth 2 attn
+sbatch finetune_retfound_UFbenchmark_v2_eval.sh DR_binary_all_split RETFound_mae RETFound_mae_natureOCT output_dir/DR_binary_all_split-IRB2024_v2-all-RETFound_mae-OCT-mcceval---testval-/checkpoint-best.pth 2 attn
+sbatch finetune_retfound_UFbenchmark_v2_eval.sh Glaucoma_binary_all_split RETFound_mae RETFound_mae_natureOCT output_dir/Glaucoma_binary_all_split-IRB2024_v2-all-RETFound_mae-OCT-mcceval---testval-/checkpoint-best.pth 2 attn
+
+## Grad-CAM-based XAI
+#sbatch finetune_retfound_UFbenchmark_v2_eval.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT output_dir/AMD_all_split-IRB2024_v2-all-RETFound_mae-OCT-mcceval---testval-/checkpoint-best.pth 2 gradcam
--- finetune_retfound_UFbenchmark_v2_eval.sh
+++ finetune_retfound_UFbenchmark_v2_eval.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem-per-cpu=4gb
 #SBATCH --partition=hpg-turin
 #SBATCH --gpus=1
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=%x.%j.out
 
 date;hostname;pwd
@@ -19,7 +19,7 @@ MODEL=${2:-"RETFound_mae"}
 FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
 RESUME=${4:-"0"} # resume path
 Num_CLASS=${5:-"2"} # 2 for AMD, 5 for DR, 5 for Glaucoma, 2 for Cataract
-XAI=${6:-"attn"}
+XAI=${6:-"attn"} # attn, rise, gradcam
 NUM_K="0"
 
 MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
@@ -29,4 +29,4 @@ echo $Num_CLASS
 
 # Modify the path to your singularity container 
 # sbatch finetune_retfound_UFbenchmark_v2_eval.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT output_dir/AMD_all_split-all-RETFound_mae--/checkpoint-best.pth 2 attn
-TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 16     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/new_v2/split/tune5-eval5/${STUDY}.csv     --task $STUDY-v2-all-$FINETUNED_MODEL-XAI${XAI}-EVAL/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K --resume $RESUME --xai $XAI
+TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 16     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/IRB2024_v2/split/tune5-eval5/${STUDY}.csv     --task $STUDY-v2-all-$FINETUNED_MODEL-XAI${XAI}-EVAL/ --img_dir /orange/ruogu.fang/tienyuchang/all_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K --resume $RESUME --xai $XAI
