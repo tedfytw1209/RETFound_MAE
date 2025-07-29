616b9f5ba79c2d2e1ff37f26a75a08662b2e3e14
teddy <teddytw1209@gmail.com>
2025-07-29 12:08:35 -0400

    Update finetune_retfound_adcon_irb2024_imgf.sh

--- finetune_retfound_adcon_irb2024_imgf.sh
+++ finetune_retfound_adcon_irb2024_imgf.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4gb
-#SBATCH --partition=gpu
-#SBATCH --gpus=a100:1
-#SBATCH --time=48:00:00
+#SBATCH --partition=hpg-turin
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
 #SBATCH --output=%x.%j.out
 #SBATCH --account=ruogu.fang
 #SBATCH --qos=ruogu.fang
@@ -35,5 +35,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_adcon_irb2024_imgf.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 100 2 0 --bal_sampler
+# sbatch finetune_retfound_adcon_irb2024_imgf.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 100 2 default OCT --bal_sampler
 torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune.py --savemodel --global_pool --batch_size 16 --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --layer_decay 0.65 --weight_decay 0.05 --drop_path 0.2 --nb_classes $Num_CLASS --data_path /blue/ruogu.fang/tienyuchang/IRB2024_DL_data_imgf/${STUDY}.csv --task $STUDY-${data_type}-all-$MODEL-${Modality}-${Eval_score}eval-$ADDCMD-$ADDCMD2/ --img_dir /orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k 0 $ADDCMD $ADDCMD2
