89c956d356bba1f4efc151245ab02bee13623cad
teddy <teddytw1209@gmail.com>
2025-08-23 00:12:45 -0400

    Update infernce_retfound_UFirb2024v4_dualvit.sh

--- infernce_retfound_UFirb2024v4_dualvit.sh
+++ infernce_retfound_UFirb2024v4_dualvit.sh
@@ -40,8 +40,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_UFbenchmark_irb2024v4.sh DR_all_split RETFound_mae RETFound_mae_natureCFP 5e-4 5
-# sbatch finetune_retfound_UFbenchmark_irb2024v4.sh Glaucoma_all_split RETFound_mae RETFound_mae_natureOCT 5e-4 6 0.05 default OCT
-# sbatch finetune_retfound_UFbenchmark_irb2024v4.sh AMD_all_split RETFound_dinov2 RETFound_dinov2_meh 5e-4 2
-# sbatch finetune_retfound_UFbenchmark_irb2024v4.sh Cataract_all_split pytorchvit B_16_imagenet1k 5e-4 6 0.05 mcc OCT --testval
+# sbatch infernce_retfound_UFirb2024v4_dualvit.sh AMD_all_split output_dir/AMD_all_split-IRB2024_v4-all-RETFound_mae_natureOCT-OCT-bs16ep50lr5e-4optadamw-roc_auceval--/checkpoint-best.pth output_dir/AMD_all_split-IRB2024_v4-all-RETFound_mae_natureCFP-CFP-bs16ep50lr5e-4optadamw-roc_auceval--/checkpoint-best.pth
 torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_dualmodel_inference.py --savemodel --global_pool --batch_size $BATCH_SIZE --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --optimizer $OPTIMIZER --layer_decay 0.65 --weight_decay $weight_decay --drop_path 0.2 --nb_classes $Num_CLASS --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/${data_type}/split/tune5-eval5/${STUDY}.csv --task $STUDY-${data_type}-all-$FINETUNED_MODEL-${Modality}-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval/ --img_dir $IMG_Path --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k $NUM_K --eval_score $Eval_score --modality $Modality --oct_resume $OCT_MODEL --cfp_resume $CFP_MODEL
