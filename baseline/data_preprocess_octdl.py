b5fac1cdbdf4ef168d0be55388fb9870ae2d06d5
Tianhao Li <144490105+skysky1997@users.noreply.github.com>
2025-09-08 07:16:04 -0500

    Add OCTDL

---
+++ baseline/data_preprocess_octdl.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+amd = os.listdir('OCTDL/OCTDL/AMD')
+dme = os.listdir('OCTDL/OCTDL/DME')
+no = os.listdir('OCTDL/OCTDL/NO')
+amd.sort()
+dme.sort()
+no.sort()
+
+train_amd, test_amd = train_test_split(amd, test_size=0.2, random_state=42)
+train_dme, test_dme = train_test_split(dme, test_size=0.2, random_state=42)
+train_no, test_no = train_test_split(no, test_size=0.2, random_state=42)
+
+train_amd_path = ['OCTDL/OCTDL/AMD/'+i for i in train_amd] + ['OCTDL/OCTDL/NO/'+i for i in train_no]
+train_amd_label = [1]*len(train_amd) + [0]*len(train_no)
+test_amd_path = ['OCTDL/OCTDL/AMD/'+i for i in test_amd] + ['OCTDL/OCTDL/NO/'+i for i in test_no]
+test_amd_label = [1]*len(test_amd) + [0]*len(test_no)
+
+train_dme_path = ['OCTDL/OCTDL/DME/'+i for i in train_dme] + ['OCTDL/OCTDL/NO/'+i for i in train_no]
+train_dme_label = [1]*len(train_dme) + [0]*len(train_no)
+test_dme_path = ['OCTDL/OCTDL/DME/'+i for i in test_dme] + ['OCTDL/OCTDL/NO/'+i for i in test_no]
+test_dme_label = [1]*len(test_dme) + [0]*len(test_no)
+
+def create_csv(image, label, name):
+    data = {
+        'image': image,
+        'label': label
+    }
+    df = pd.DataFrame(data)
+    df.to_csv(name, index=False)
+
+create_csv(train_amd_path, train_amd_label, 'OCTDL/amd_train.csv')
+create_csv(test_amd_path, test_amd_label, 'OCTDL/amd_test.csv')
+create_csv(train_dme_path, train_dme_label, 'OCTDL/dme_train.csv')
+create_csv(test_dme_path, test_dme_label, 'OCTDL/dme_test.csv')
