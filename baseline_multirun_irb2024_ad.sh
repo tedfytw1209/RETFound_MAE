7347eb57619be4f8859c7ff6a57add86e0a73a89
teddy <teddytw1209@gmail.com>
2025-09-16 15:24:56 -0400

    Update baseline_multirun_irb2024_ad.sh

--- baseline_multirun_irb2024_ad.sh
+++ baseline_multirun_irb2024_ad.sh
@@ -8,14 +8,15 @@ Epochs=${5:-"100"}
 Num_CLASS=${6:-"2"}
 Eval_score=${7:-"default"}
 Modality=${8:-"OCT"} # CFP, OCT, OCT_CFP
-ADDCMD=${9:-""}
-ADDCMD2=${10:-""}
+SUBSETNUM=${9:-0} # 0, 500, 1000
+ADDCMD=${10:-""}
+ADDCMD2=${11:-""}
 
 NUM_K=0
 
 #bash baseline_multirun_irb2024_ad.sh finetune_retfound_adcon_irb2024_imgf.sh RETFound_mae RETFound_mae_natureOCT 5e-4 50 2 default OCT --bal_sampler
 #bash baseline_multirun_irb2024_ad.sh finetune_retfound_adcon_irb2024_imgf.sh efficientnet-b4 google/efficientnet-b4 5e-4 50 2 default OCT --bal_sampler
-#bash baseline_multirun_irb2024_ad.sh finetune_retfound_adcon_irb2024_imgf.sh vit-base-patch16-224 google/vit-base-patch16-224 5e-4 50 2 default OCT --bal_sampler
+#bash baseline_multirun_irb2024_ad.sh finetune_retfound_adcon_irb2024_imgf.sh vit-base-patch16-224 google/vit-base-patch16-224 5e-4 50 2 roc_auc OCT --bal_sampler
 #bash baseline_multirun_irb2024_ad.sh finetune_retfound_adcon_irb2024_imgf.sh resnet-50 microsoft/resnet-50 5e-4 50 2 default OCT --bal_sampler
 DATASETS=(ad_mci_control_detect_data ad_mci_detect_data mci_control_detect_data)  # List of datasets
 CLASSES=(3 2 2)  # Number of classes for each dataset
@@ -26,6 +27,6 @@ do
     NUM_CLASS="${CLASSES[$i]}"
     echo "Running dataset: $DATASET with num_class=$NUM_CLASS"
     # Submit the job to Slurm
-    echo "sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $Epochs $NUM_CLASS $Eval_score $Modality $ADDCMD $ADDCMD2"
-    sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $Epochs $NUM_CLASS $Eval_score $Modality $ADDCMD $ADDCMD2
+    echo "sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $Epochs $NUM_CLASS $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2"
+    sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $Epochs $NUM_CLASS $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2
 done
