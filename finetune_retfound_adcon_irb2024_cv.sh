20fa8b47a4b5a849b5f1fe5d58b45e5bb013d730
teddy <teddytw1209@gmail.com>
2025-09-16 18:07:28 -0400

    Update finetune_retfound_adcon_irb2024_cv.sh

--- finetune_retfound_adcon_irb2024_cv.sh
+++ finetune_retfound_adcon_irb2024_cv.sh
@@ -36,5 +36,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_adcon_irb2024_cv.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 30 2 default OCT 4 --bal_sampler
+# sbatch finetune_retfound_adcon_irb2024_cv.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 50 2 default OCT 4 --bal_sampler
 torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune_cv.py --savemodel --global_pool --batch_size 16 --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --layer_decay 0.65 --weight_decay 0.05 --drop_path 0.2 --nb_classes $Num_CLASS --data_path /blue/ruogu.fang/tienyuchang/${data_type}/${STUDY}.csv --task $STUDY-${data_type}-all-$MODEL-${Modality}-${Eval_score}eval-$ADDCMD-$ADDCMD2-CV/ --eval_score $Eval_score --modality $Modality --subset_ratio $SUBSET_R --img_dir /orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 16 --input_size 224 --num_k 0 $ADDCMD $ADDCMD2
