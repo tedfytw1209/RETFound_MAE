bb6f1e84d2f47bdc637113ce02a0e0e0c85de352
teddy <teddytw1209@gmail.com>
2025-09-21 10:52:18 -0400

    Update Attention.py

--- baselines/Attention.py
+++ baselines/Attention.py
@@ -112,6 +112,10 @@ class Attention_Map(torch.nn.Module):
                 outputs = self.model(x, output_attentions=True, return_dict=True)
                 if hasattr(outputs, "attentions"):
                     attentions = outputs.attentions #(B, n_heads, num_tokens, num_tokens)
+                else:
+                    print("Model does not return attentions. Please check the model.")
+                    print(outputs)
+                    print(self.model)
 
         attention_maps = generate_attention_map_batch(attentions, img_size=self.input_size, use_rollout=self.use_rollout)
         #attention_maps = torch.from_numpy(attention_maps).float().cuda()
