be1feb46057087101751fe7219f81bf73621f2cc
teddy <teddytw1209@gmail.com>
2025-10-19 19:11:25 -0400

    script update

--- finetune_retfound_adcon_irb2024_v5.sh
+++ finetune_retfound_adcon_irb2024_v5.sh
@@ -19,9 +19,10 @@ conda activate retfound_new
 STUDY=$1
 MODEL=${2:-"RETFound_mae"}
 FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
-LR=${4:-"1e-3"}
-Epochs=${5:-"100"}
-Num_CLASS=${6:-"2"}
+LR=${4:-"5e-4"}
+Epochs="50"
+Num_CLASS=${5:-"2"}
+weight_decay=${6:-"0.05"}
 Eval_score=${7:-"default"}
 Modality=${8:-"OCT"} # CFP, OCT, OCT_CFP
 SUBSETNUM=${9:-0} # 0, 500, 1000
@@ -36,5 +37,5 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_adcon_irb2024_v5.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 50 2 default OCT 0 --bal_sampler
-torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune.py --savemodel --global_pool --batch_size 16 --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --layer_decay 0.65 --weight_decay 0.05 --drop_path 0.2 --nb_classes $Num_CLASS --data_path /blue/ruogu.fang/tienyuchang/${data_type}/${STUDY}.csv --task $STUDY-${data_type}-all-$MODEL-${Modality}-${Eval_score}eval-trsub${SUBSETNUM}-$ADDCMD-$ADDCMD2/ --eval_score $Eval_score --modality $Modality --img_dir /orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k 0 --new_subset_num $SUBSETNUM $ADDCMD $ADDCMD2
+# sbatch finetune_retfound_adcon_irb2024_v5.sh ad_control_detect_data RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 roc_auc OCT 0 --bal_sampler
+torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune.py --savemodel --global_pool --batch_size 16 --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --layer_decay 0.65 --weight_decay $weight_decay --drop_path 0.2 --nb_classes $Num_CLASS --data_path /blue/ruogu.fang/tienyuchang/${data_type}/${STUDY}.csv --task $STUDY-${data_type}-all-$MODEL-${Modality}-${Eval_score}eval-trsub${SUBSETNUM}-$ADDCMD-$ADDCMD2/ --eval_score $Eval_score --modality $Modality --img_dir /orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size 224 --num_k 0 --new_subset_num $SUBSETNUM $ADDCMD $ADDCMD2
