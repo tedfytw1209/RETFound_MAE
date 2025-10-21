b09e7457ff0744fa389b062e4f0ba5feb9abb05e
teddy <teddytw1209@gmail.com>
2025-10-20 21:52:31 -0400

    add np.mean to pass assert

--- main_XAI_evaluation.py
+++ main_XAI_evaluation.py
@@ -329,9 +329,9 @@ def evaluate_XAI(data_loader, xai_method, metric_func_dict, device, args, epoch,
         attention_map_bs = xai_method(images) # numpy shape: (B, img_size, img_size)
         print(f'Attention map shape: {attention_map_bs.shape}')
         for k, v in metric_func_dict.items():
-            e_score = v(images, attention_map_bs, batch_size=bs, y_batch=target, explain_func=xai_method, explain_func_kwargs={})
-            overall_metrics_dict[k].append(e_score)
-            each_dict[k] = e_score
+            e_score_bs = np.mean(v(images, attention_map_bs, batch_size=bs, y_batch=target, explain_func=xai_method, explain_func_kwargs={}))
+            overall_metrics_dict[k].append(e_score_bs)
+            each_dict[k] = e_score_bs
             
         metric_logger.update(**each_dict)
     
