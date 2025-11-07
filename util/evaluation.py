8d5cfcfba7e4a5ce544eb3ecbbbcdd314594dbe4
teddy <teddytw1209@gmail.com>
2025-11-07 18:19:47 -0500

    init exp metric

--- util/evaluation.py
+++ util/evaluation.py
@@ -325,3 +325,69 @@ class DeletionMetric(CausalMetric):
         h = self.evaluate(img_batch, exp_batch, batch_size)
         return auc(h.mean(1))
 
+def pool_heatmap (heatmap: np.ndarray, pooling_type: str) -> np.ndarray:
+    """
+    Pool the relevance along the channel axis, according to the pooling technique specified by pooling_type.
+    """
+    C, H, W = heatmap.shape
+
+    if pooling_type=="sum,abs":
+        pooled_heatmap = np.abs(np.sum(heatmap, axis=0))
+
+    elif pooling_type=="sum,pos":
+        pooled_heatmap = np.sum(heatmap, axis=0) ; pooled_heatmap = np.where(pooled_heatmap>0.0, pooled_heatmap, 0.0)
+    
+    elif pooling_type=="max-norm":
+        pooled_heatmap = np.amax(np.abs(heatmap), axis=0)
+
+    elif pooling_type=="l1-norm":
+        pooled_heatmap = np.linalg.norm(heatmap, ord=1, axis=0)
+
+    elif pooling_type=="l2-norm":
+        pooled_heatmap = np.linalg.norm(heatmap, ord=2, axis=0)
+
+    elif pooling_type=="l2-norm,sq":
+        pooled_heatmap = (np.linalg.norm(heatmap, ord=2, axis=0))**2
+
+    assert pooled_heatmap.shape == (H, W) and np.all(pooled_heatmap>=0.0)
+    return pooled_heatmap
+
+def evaluate_single(heatmap: np.ndarray, ground_truth: np.ndarray, pooling_type: str):
+    """
+    Given an image's relevance heatmap and a corresponding ground truth boolean ndarray of the same vertical and horizontal dimensions, return both:
+     - the ratio of relevance falling into the ground truth area w.r.t. the total amount of relevance ("relevance mass accuracy" metric)
+     - the ratio of pixels within the N highest relevant pixels (where N is the size of the ground truth area) that effectively belong to the ground truth area
+       ("relevance rank accuracy" metric)
+    Both ratios are calculated after having pooled the relevance across the channel axis, according to the pooling technique defined by the pooling_type argument.
+    Args:
+    - heatmap (np.ndarray):         of shape (C, H, W), with dtype float 
+    - ground_truth (np.ndarray):    of shape (H, W), with dtype bool
+    - pooling_type (str):           specifies how to pool the relevance across the channels, i.e. defines a mapping function f: R^C -> R^+
+                                    that maps a real-valued vector of dimension C to a positive number (see details of each pooling_type in the function pool_heatmap)
+    Returns:
+    A dict wich keys=["mass", "rank"] and resp. values:
+    - relevance_mass_accuracy (np.float64):     relevance mass accuracy, float in the range [0.0, 1.0], the higher the better.
+    - relevance_rank_accuracy (np.float64):     relevance rank accuracy, float in the range [0.0, 1.0], the higher the better.
+    """
+    C, H, W = heatmap.shape # C relevance values per pixel coordinate (C=number of channels), for an image with vertical and horizontal dimensions HxW
+    assert ground_truth.shape == (H, W)
+
+    heatmap = heatmap.astype(dtype=np.float64) # cast heatmap to float64 precision (better for computing relevance accuracy statistics)
+    
+    # step 1: pool the relevance across the channel dimension to get one positive relevance value per pixel coordinate
+    pooled_heatmap = pool_heatmap(heatmap, pooling_type)
+
+    # step 2: compute the ratio of relevance mass within ground truth w.r.t the total relevance
+    relevance_within_ground_truth = np.sum(pooled_heatmap * np.where(ground_truth, 1.0, 0.0).astype(dtype=np.float64) )
+    relevance_total               = np.sum(pooled_heatmap)
+    relevance_mass_accuracy       = 1.0 * relevance_within_ground_truth/relevance_total ; assert (0.0<=relevance_mass_accuracy) and (relevance_mass_accuracy<=1.0)
+
+    # step 3: order the pixel coordinates in decreasing order of their relevance, then count the number N_gt of pixels within the N highest relevant pixels that fall 
+    # into the ground truth area, where N is the total number of pixels of the ground truth area, then compute the ratio N_gt/N
+    pixels_sorted_by_relevance = np.argsort(np.ravel(pooled_heatmap))[::-1] ; assert pixels_sorted_by_relevance.shape == (H*W,) # sorted pixel indices over flattened array
+    gt_flat = np.ravel(ground_truth)                                        ; assert gt_flat.shape == (H*W,) # flattened ground truth array
+    N    = np.sum(gt_flat)  
+    N_gt = np.sum(gt_flat[pixels_sorted_by_relevance[:int(N)]])
+    relevance_rank_accuracy = 1.0 * N_gt/N ; assert (0.0<=relevance_rank_accuracy) and (relevance_rank_accuracy<=1.0)
+        
+    return {"mass": relevance_mass_accuracy, "rank": relevance_rank_accuracy} # dict of relevance accuracies, with key=evaluation metric
