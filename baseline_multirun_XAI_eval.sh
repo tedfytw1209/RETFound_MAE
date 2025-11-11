1995ad29a00ff6c0e713eb6ff7f52aa72f78e036
teddy <teddytw1209@gmail.com>
2025-11-11 09:41:49 -0500

    Update baseline_multirun_XAI_eval.sh

--- baseline_multirun_XAI_eval.sh
+++ baseline_multirun_XAI_eval.sh
@@ -10,7 +10,7 @@
 #SBATCH --account=ruogu.fang
 #SBATCH --qos=ruogu.fang
 
-SCRIPT=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 6, Glaucoma_all_split 6, DR_binary_all_split 2, Glaucoma_binary_all_split 2
+SCRIPT=$1
 MODEL=${2:-"RETFound_mae"}
 FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
 INPUT_SIZE=${4:-224}
@@ -20,7 +20,7 @@ MODEL_DIR="/orange/ruogu.fang/tienyuchang/RETfound_results"
 #microsoft/resnet-50, timm_efficientnet-b4, google/vit-base-patch16-224-in21k, RETFound_mae_natureOCT
 #DATASETS=(AMD_all_split DR_binary_all_split DME_binary_all_split)  # List of datasets
 #CLASSES=(2 2 2)  # Number of classes for each dataset
-DATASETS=(DME_binary_all_split)  # List of datasets
+DATASETS=(AMD_all_split)  # List of datasets
 CLASSES=(2)  # Number of classes for each dataset
 
 #sbatch baseline_multirun_XAI_eval.sh finetune_retfound_UFbenchmark_v5_eval.sh RETFound_mae RETFound_mae_natureOCT 224
