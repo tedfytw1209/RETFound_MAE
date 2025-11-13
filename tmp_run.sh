c337a8fd5e90c790963af60ddbc1e83ba38f279e
teddy <teddytw1209@gmail.com>
2025-11-12 23:58:47 -0500

    Update tmp_run.sh

--- tmp_run.sh
+++ tmp_run.sh
@@ -10,6 +10,4 @@
 #SBATCH --account=ruogu.fang
 #SBATCH --qos=ruogu.fang
 
-bash finetune_retfound_UFbenchmark_v5_eval_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_binary_all_split-IRB2024_v5-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-enc---add_mask---train_no_aug/checkpoint-best.pth 2 512 crp 1024 enc
-
-bash finetune_retfound_UFbenchmark_v5_eval_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_binary_all_split-IRB2024_v5-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0---add_mask---train_no_aug/checkpoint-best.pth 2 512 crp 1024 dec
+bash finetune_retfound_UFbenchmark_v5_eval_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_binary_all_split-IRB2024_v5-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0--/checkpoint-best.pth 2 512 crp 1024 dec
