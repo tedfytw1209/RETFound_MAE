b952875902f2c52cc43eb5513a9af70013aa62a3
teddy <teddytw1209@gmail.com>
2025-11-30 16:52:05 -0500

    Create inference_mask.sh

---
+++ SMP/inference_mask.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --partition=hpg-turin
+#SBATCH --gpus=1
+#SBATCH --time=12:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --account=ruogu.fang
+#SBATCH --qos=ruogu.fang
+
+date;hostname;pwd
+
+module load conda
+conda activate retfound_new
+# Go to home directory
+#cd $HOME
+DATASET=$1
+STUDY=$2
+MAX_IMAGES=$3
+
+python inference_general.py --dataset $DATASET --study $STUDY --max-images $MAX_IMAGES
