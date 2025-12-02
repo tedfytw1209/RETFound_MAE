b3a25a51416d405d1f6dd2ee145ec639325290a8
teddy <teddytw1209@gmail.com>
2025-12-01 22:42:09 -0500

    Update main_finetune_smp.py

--- main_finetune_smp.py
+++ main_finetune_smp.py
@@ -222,9 +222,7 @@ def get_args_parser():
                         help='SMP fuse mode ("weighted_sum", "add", "channel_merge", "channel_multiply", "multiply") (default: "weighted_sum")')
     parser.add_argument('--smp_learnable_alpha', action='store_true', default=False,
                         help='SMP learnable alpha (default: False)')
-    parser.add_argument('--smp_alpha', type=float, default=0.5,
-                        min_value=0.0, max_value=1.0,
-                        help='SMP alpha (0.0-1.0)')
+    parser.add_argument('--smp_alpha', type=float, default=0.5,help='SMP alpha (0.0-1.0)')
     parser.add_argument('--smp_size_match', type=str, default='decoder_to_encoder',
                         choices=["decoder_to_encoder", "encoder_to_decoder"],
                         help='SMP size match (decoder_to_encoder, encoder_to_decoder) (default: "decoder_to_encoder")')
