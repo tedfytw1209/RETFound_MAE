9f65543a0d0fd4522370fce7b24a7febe6587585
teddy <teddytw1209@gmail.com>
2025-12-08 23:10:21 -0500

    script

--- finetune_retfound_UFbenchmark_irb2024v5_smp.sh
+++ finetune_retfound_UFbenchmark_irb2024v5_smp.sh
@@ -30,9 +30,11 @@ SMPFuseMode=${11:-"weighted_sum"} # ("weighted_sum", "add", "channel_merge", "ch
 SMPAlpha=${12:-0.5} # 0.0-1.0
 SMPSizeMatch=${13:-"decoder_to_encoder"} # decoder_to_encoder, encoder_to_decoder
 FUSION_DIM=${14:-0} # 0 for default
-ADDCMD=${15:-""}
-ADDCMD2=${16:-""}
-ADDCMD3=${17:-""}
+ENC_IDX=${15:-"-1"} # -1 for last encoder layer
+DEC_IDX=${16:-"-1"} # -1 for last decoder layer
+ADDCMD=${17:-""}
+ADDCMD2=${18:-""}
+ADDCMD3=${19:-""}
 
 NUM_K=0
 data_type="IRB2024_v5"
@@ -47,6 +49,6 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_UFbenchmark_irb2024v5_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 1e-4 2 1e-4 default OCT 0 fuse weighted_sum 0.5 decoder_to_encoder 0 --smp_learnable_alpha
-# sbatch finetune_retfound_UFbenchmark_irb2024v5_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 1e-4 2 1e-4 default OCT 0 fuse 0 --add_mask --train_no_aug --fix_extractor
-torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune_smp.py --savemodel --global_pool --batch_size $BATCH_SIZE --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --optimizer $OPTIMIZER --layer_decay 0.65 --weight_decay $weight_decay --drop_path 0.0 --nb_classes $Num_CLASS --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/${data_type}/split/tune5-eval5/${STUDY}.csv --task $STUDY-${data_type}-all-$FINETUNED_MODEL-${Modality}-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval-trsub${SUBSETNUM}-${SMPMode}-smp${SMPFuseMode}-${SMPAlpha}-${SMPSizeMatch}-$ADDCMD-$ADDCMD2-$ADDCMD3/ --img_dir $IMG_Path --log_dir /orange/ruogu.fang/tienyuchang/RETfound_results --output_dir /orange/ruogu.fang/tienyuchang/RETfound_results --finetune $FINETUNED_MODEL --num_workers 8 --input_size 512 --num_k $NUM_K --eval_score $Eval_score --modality $Modality --visualize_samples --new_subset_num $SUBSETNUM --SMPMode $SMPMode --smp_fuse_mode $SMPFuseMode --fusion_dim $FUSION_DIM --smp_alpha $SMPAlpha --smp_size_match $SMPSizeMatch $ADDCMD $ADDCMD2 $ADDCMD3
+# sbatch finetune_retfound_UFbenchmark_irb2024v5_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 1e-4 2 1e-4 default OCT 0 fuse weighted_sum 0.5 decoder_to_encoder 0 -1 -1 --smp_learnable_alpha
+# sbatch finetune_retfound_UFbenchmark_irb2024v5_smp.sh DME_binary_all_split SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 1e-4 2 1e-4 default OCT 0 fuse 0 -1 -1 --add_mask --train_no_aug --fix_extractor
+torchrun --nproc_per_node=1 --master_port=$MASTER_PORT main_finetune_smp.py --savemodel --global_pool --batch_size $BATCH_SIZE --world_size 1 --model $MODEL --epochs $Epochs --lr $LR --optimizer $OPTIMIZER --layer_decay 0.65 --weight_decay $weight_decay --drop_path 0.0 --nb_classes $Num_CLASS --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/${data_type}/split/tune5-eval5/${STUDY}.csv --task $STUDY-${data_type}-all-$FINETUNED_MODEL-${Modality}-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval-trsub${SUBSETNUM}-${SMPMode}-smp${SMPFuseMode}-${SMPAlpha}-${SMPSizeMatch}-$ADDCMD-$ADDCMD2-$ADDCMD3/ --img_dir $IMG_Path --log_dir /orange/ruogu.fang/tienyuchang/RETfound_results --output_dir /orange/ruogu.fang/tienyuchang/RETfound_results --finetune $FINETUNED_MODEL --num_workers 8 --input_size 512 --num_k $NUM_K --eval_score $Eval_score --modality $Modality --visualize_samples --new_subset_num $SUBSETNUM --SMPMode $SMPMode --smp_fuse_mode $SMPFuseMode --fusion_dim $FUSION_DIM --smp_alpha $SMPAlpha --smp_size_match $SMPSizeMatch --enc_idx $ENC_IDX --dec_idx $DEC_IDX $ADDCMD $ADDCMD2 $ADDCMD3
