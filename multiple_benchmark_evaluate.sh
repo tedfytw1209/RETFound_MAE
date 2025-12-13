fb4657e5fc649be6856975e5885df547b84a5d14
teddy <teddytw1209@gmail.com>
2025-12-13 12:02:04 -0500

    Update multiple_benchmark_evaluate.sh

--- multiple_benchmark_evaluate.sh
+++ multiple_benchmark_evaluate.sh
@@ -14,7 +14,7 @@ CKPT_ROOT="/orange/ruogu.fang/tienyuchang/RETFound_ckpt"
 
 bash finetune_retfound_benchmark.sh APTOS2019 RETFound_mae 5 RETFound_mae_natureCFP $CKPT_ROOT/APTOS2019/checkpoint-best.pth --eval
 bash finetune_retfound_benchmark.sh MESSIDOR2 RETFound_mae 5 RETFound_mae_natureCFP $CKPT_ROOT/MESSIDOR2/checkpoint-best.pth --eval
-bash finetune_retfound_benchmark.sh IDRID_data RETFound_mae 5 RETFound_mae_natureCFP $CKPT_ROOT/IDRID/checkpoint-best.pth --eval
+bash finetune_retfound_benchmark.sh IDRiD_data RETFound_mae 5 RETFound_mae_natureCFP $CKPT_ROOT/IDRID/checkpoint-best.pth --eval
 bash finetune_retfound_benchmark.sh PAPILA RETFound_mae 3 RETFound_mae_natureCFP $CKPT_ROOT/PAPILA/checkpoint-best.pth --eval
 bash finetune_retfound_benchmark.sh Glaucoma_fundus RETFound_mae 3 RETFound_mae_natureCFP $CKPT_ROOT/Glaucoma/checkpoint-best.pth --eval
 bash finetune_retfound_benchmark.sh JSIEC RETFound_mae 39 RETFound_mae_natureCFP $CKPT_ROOT/JSIEC/checkpoint-best.pth --eval
