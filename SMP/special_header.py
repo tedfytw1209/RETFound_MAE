30a5e358db6826906ac3a9e6c1ff1fdf63377a5a
teddy <teddytw1209@gmail.com>
2026-01-07 21:13:12 +0800

    Update special_header.py

--- SMP/special_header.py
+++ SMP/special_header.py
@@ -665,10 +665,17 @@ class GeneralFusionHead(nn.Module):
             fused = self._channel_multiply(enc_feats, dec_feats)
         else:
             fused = self._merge(enc_feats, dec_feats)
-        #print("Fused feature shape:", fused.shape)
-        pooled = self._pool(fused)
-        #print("Pooled feature shape:", pooled.shape)
-        logits = self.classifier(pooled)
+        print("Fused feature shape:", fused.shape)
+        if self.smp_classifier == "linear":
+            fused = self.dropout(fused)
+            pooled = self._pool(fused)
+            #print("Pooled feature shape:", pooled.shape)
+            logits = self.classifier(pooled)
+        else:  # conv classifier
+            logits_map = self.classifier(self.dropout(fused))
+            print("Conv feature shape:", logits_map.shape)
+            logits = self._pool(logits_map)
+        print("Logits shape:", logits.shape)
         if return_fused_feature:
             return logits, fused
         return logits
