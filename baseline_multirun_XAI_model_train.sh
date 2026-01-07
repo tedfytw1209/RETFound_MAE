c11a89663a82226b4ac66dda11e53922d27188dd
teddy <teddytw1209@gmail.com>
2026-01-07 19:38:51 +0800

    update script

--- baseline_multirun_XAI_model_train.sh
+++ baseline_multirun_XAI_model_train.sh
@@ -20,8 +20,9 @@ weight_decay=${7:-"0.05"}
 Eval_score=${8:-"default"}
 Modality=${9:-"OCT"} # CFP, OCT, OCT_CFP
 SUBSETNUM=0 # 0, 500, 1000
-ADDCMD=""
-ADDCMD2=""
+ADDCMD=${10:-""} #
+ADDCMD2=${11:-""} #
+ADDCMD3=${12:-""} #
 
 #sbatch baseline_multirun_XAI_model_train.sh finetune_retfound_UFbenchmark_irb2024v5.sh DME_binary_all_split RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 default OCT
 #sbatch baseline_multirun_XAI_model_train.sh finetune_retfound_UFbenchmark_irb2024v5_init.sh DME_binary_all_split RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 default OCT
@@ -29,10 +30,10 @@ ADDCMD2=""
 #sbatch baseline_multirun_XAI_model_train.sh finetune_retfound_Celldata.sh DME_all RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 default OCT
 #sbatch baseline_multirun_XAI_model_train.sh finetune_retfound_OCTDL.sh DME_all RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 default OCT
 #sbatch baseline_multirun_XAI_model_train.sh finetune_retfound_OCTIDdata.sh DR_all RETFound_mae RETFound_mae_natureOCT 5e-4 2 0.05 default OCT
-#MODELS=(timm_efficientnet-b4 resnet-50 vit-base-patch16-224 RETFound_mae)  # List of models
-#FINETUNED_MODELS=(timm_efficientnet-b4 microsoft/resnet-50 google/vit-base-patch16-224-in21k RETFound_mae_natureOCT)  # Number of classes for each dataset
-MODELS=(RETFound_mae)  # List of models
-FINETUNED_MODELS=(RETFound_mae_natureOCT)  # Number of classes for each dataset
+MODELS=(timm_efficientnet-b4 resnet-50 vit-base-patch16-224 RETFound_mae)  # List of models
+FINETUNED_MODELS=(timm_efficientnet-b4 microsoft/resnet-50 google/vit-base-patch16-224-in21k RETFound_mae_natureOCT)  # Number of classes for each dataset
+#MODELS=(RETFound_mae)  # List of models
+#FINETUNED_MODELS=(RETFound_mae_natureOCT)  # Number of classes for each dataset
 for i in "${!MODELS[@]}"
 do
     #for j in "${!ADDCMDS1[@]}"
@@ -43,7 +44,7 @@ do
     #ADDCMD="${ADDCMDS1[$j]}"
     #ADDCMD2="${ADDCMDS2[$j]}"
     # Submit the job to Slurm
-    echo "bash $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $NUM_CLASS $weight_decay $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2"
-    bash $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $NUM_CLASS $weight_decay $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2
+    echo "bash $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $NUM_CLASS $weight_decay $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2 $ADDCMD3"
+    bash $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $LR $NUM_CLASS $weight_decay $Eval_score $Modality $SUBSETNUM $ADDCMD $ADDCMD2 $ADDCMD3
     #done
 done
