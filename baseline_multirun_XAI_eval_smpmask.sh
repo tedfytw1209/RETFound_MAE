dbcea6ccf92493c6aab4dc431e9fdec81fc18e7d
teddy <teddytw1209@gmail.com>
2026-01-08 15:48:05 +0800

    debug

--- baseline_multirun_XAI_eval_smpmask.sh
+++ baseline_multirun_XAI_eval_smpmask.sh
@@ -21,6 +21,9 @@ SMPSizeMatch=${8:-"decoder_to_encoder"} # decoder_to_encoder, encoder_to_decoder
 FUSION_DIM=${9:-0} # 0 for default
 ENC_IDX=${10:-"-1"} # -1 for last encoder layer
 DEC_IDX=${11:-"-1"} # -1 for last decoder layer
+TARGET_MODULE=${12:-"encoder"} # encoder, decoder, head
+SELECT_INDEX=${13:-"-1"} # -1 for last layer
+ADDCMD3=${14:-""}
 
 NUM_K=0
 MODEL_DIR="/orange/ruogu.fang/tienyuchang/RETfound_results"
@@ -31,10 +34,10 @@ DATASETS=(DME_binary_all_split)  # List of datasets
 CLASSES=(2)  # Number of classes for each dataset
 STEP_PIXELS=1024
 Thickness_DIR="/orange/ruogu.fang/tienyuchang/IRB2024_OCT_thickness/Data/"
-ADD_WORDS="--add_mask"
-ADD_WORDS2="--smp_learnable_alpha"
+ADDCMD="--add_mask"
+ADDCMD2="--smp_learnable_alpha"
 
-#bash baseline_multirun_XAI_eval_smpmask.sh finetune_retfound_UFbenchmark_v5_eval_smp.sh SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 512 enc
+#bash baseline_multirun_XAI_eval_smpmask.sh finetune_retfound_UFbenchmark_v5_eval_smp.sh SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth 512 enc weighted_sum 0.5 encoder_to_decoder 16 -1 -1 encoder -1
 #XAI_METHODS=("gradcamv2" "scorecam" "crp")  # List of XAI methods
 XAI_METHODS=("gradcamv2" "hirescam" "gradcam++")  # List of XAI methods
 #XAI_METHODS=("crp")  # List of XAI methods
@@ -47,7 +50,7 @@ do
     for XAI in "${XAI_METHODS[@]}"
     do
         # DME_binary_all_split-IRB2024_v5-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0--/
-        echo "sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $MODEL_DIR/$DATASET-IRB2024_v5-all-$FINETUNED_MODEL-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-$SMPMode---add_mask---train_no_aug/checkpoint-best.pth $NUM_CLASS $INPUT_SIZE $XAI $STEP_PIXELS $Thickness_DIR $SMPMode $ADD_WORDS"
-        #sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $MODEL_DIR/$DATASET-IRB2024_v5-all-$FINETUNED_MODEL-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-$SMPMode---add_mask---train_no_aug/checkpoint-best.pth $NUM_CLASS $INPUT_SIZE $XAI $STEP_PIXELS $Thickness_DIR $SMPMode $ADD_WORDS
+        echo "sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $MODEL_DIR/$DATASET-IRB2024_v5-all-$FINETUNED_MODEL-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-$SMPMode---add_mask---train_no_aug/checkpoint-best.pth $NUM_CLASS $INPUT_SIZE $XAI $STEP_PIXELS $Thickness_DIR $SMPMode $SMPFuseMode $SMPAlpha $SMPSizeMatch $FUSION_DIM $ENC_IDX $DEC_IDX $TARGET_MODULE $SELECT_INDEX $ADDCMD $ADDCMD2 $ADDCMD3"
+        #sbatch $SCRIPT $DATASET $MODEL $FINETUNED_MODEL $MODEL_DIR/$DATASET-IRB2024_v5-all-$FINETUNED_MODEL-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-$SMPMode---add_mask---train_no_aug/checkpoint-best.pth $NUM_CLASS $INPUT_SIZE $XAI $STEP_PIXELS $Thickness_DIR $SMPMode $SMPFuseMode $SMPAlpha $SMPSizeMatch $FUSION_DIM $ENC_IDX $DEC_IDX $TARGET_MODULE $SELECT_INDEX $ADDCMD $ADDCMD2 $ADDCMD3
     done
 done
