d53e81379e39402f94ad3c8d6f7ecaed914e5f3b
teddy <teddytw1209@gmail.com>
2026-01-09 14:51:03 +0800

    Update finetune_retfound_OCTDL_smpeval.sh

--- finetune_retfound_OCTDL_smpeval.sh
+++ finetune_retfound_OCTDL_smpeval.sh
@@ -24,7 +24,20 @@ Num_CLASS=${5:-"2"} # 2 for AMD, 5 for DR, 5 for Glaucoma, 2 for Cataract
 INPUT_SIZE=${6:-"224"}
 XAI=${7:-"attn"} # attn, rise, gradcam
 STEP_PIXELS=${8:-"224"}
-SMPMode=${9:-"dec"} # dec, enc, fuse
+Thickness_DIR=${9:-"/orange/ruogu.fang/tienyuchang/IRB2024_OCT_thickness/Data/"}
+SMPMode=${10:-"dec"} # dec, enc, fuse
+SMPFuseMode=${11:-"weighted_sum"} # ("weighted_sum", "add", "channel_merge", "channel_multiply", "multiply")
+SMPAlpha=${12:-0.5} # 0.0-1.0
+SMPSizeMatch=${13:-"decoder_to_encoder"} # decoder_to_encoder, encoder_to_decoder
+FUSION_DIM=${14:-0} # 0 for default
+ENC_IDX=${15:-"-1"} # -1 for last encoder layer
+DEC_IDX=${16:-"-1"} # -1 for last decoder layer
+TARGET_MODULE=${17:-"encoder"} # encoder, decoder, head
+SELECT_INDEX=${18:-"-1"} # -1 for last layer
+SMPClassifier=${19:-"linear"} # linear, conv
+ADDCMD=${20:-""}
+ADDCMD2=${21:-""}
+ADDCMD3=${22:-""}
 NUM_K="0"
 data_type="OCTDL"
 IMG_Path="/orange/ruogu.fang/tienyuchang/OCTDL/"
@@ -36,10 +49,11 @@ echo $SUBSTUDY
 echo $Num_CLASS
 
 # Modify the path to your singularity container 
-# sbatch finetune_retfound_UFbenchmark_v5_smpeval.sh DME_all SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_all-OCTDL-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-enc--/checkpoint-best.pth 2 512 gradcamv2 1024 enc
+# sbatch finetune_retfound_OCTDL_smpeval.sh DME_all SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_all-OCTDL-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass_resnet50.pth-OCT-bs4ep20lr1e-4optadamw-defaulteval-trsub0-enc-smpweighted_sum-\{0\}-fea-1-1-0.5-decoder_to_encoder-conv---/checkpoint-best.pth 2 512 hirescam 1024 /orange/ruogu.fang/tienyuchang/IRB2024_OCT_thickness/Data/ enc weighted_sum 0.5 decoder_to_encoder 0 -1 -1 encoder -1 conv
+
 #XAI_METHODS=("gradcamv2" "scorecam" "crp")  # List of XAI methods
 
 #for XAI in "${XAI_METHODS[@]}"
 #do
-TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 2     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/${data_type}/${STUDY}.csv     --task $STUDY-${data_type}-all-$FINETUNED_MODEL-XAI${XAI}-EVAL/ --img_dir $IMG_Path --thickness_dir $MASK_DIR --finetune $FINETUNED_MODEL --num_workers 8 --input_size $INPUT_SIZE --num_k $NUM_K --resume $RESUME --xai $XAI --step_pixels $STEP_PIXELS --SMPMode $SMPMode --output_mask
+TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 2     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/${data_type}/${STUDY}.csv     --task $STUDY-${data_type}-all-$FINETUNED_MODEL-XAI${XAI}-EVAL/ --img_dir $IMG_Path --thickness_dir $MASK_DIR --finetune $FINETUNED_MODEL --num_workers 8 --input_size $INPUT_SIZE --num_k $NUM_K --resume $RESUME --xai $XAI --step_pixels $STEP_PIXELS --SMPMode $SMPMode --output_mask --thickness_dir $Thickness_DIR --target_module $TARGET_MODULE --select_index $SELECT_INDEX --smp_fuse_mode $SMPFuseMode --smp_alpha $SMPAlpha --smp_size_match $SMPSizeMatch --fusion_dim $FUSION_DIM --enc_idx $ENC_IDX --dec_idx $DEC_IDX --smp_classifier $SMPClassifier $ADDCMD $ADDCMD2 $ADDCMD3 
 #done
