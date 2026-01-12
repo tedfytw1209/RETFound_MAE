4a583d9d2d73e7580de6e7205954a1d474b6c7a5
teddy <teddytw1209@gmail.com>
2026-01-12 07:10:06 -0500

    case study script

--- run_xai_layermap_bs.sh
+++ run_xai_layermap_bs.sh
@@ -19,26 +19,28 @@ conda activate octxai
 # Model Definitions
 # ============================================================================
 
+SCRIPT="case_study_SMP_layermap_bs.py"
+STUDY="DME"
+MODEL=${1:-"RETFound_mae"}
+FINETUNED_MODEL=${2:-"RETFound_mae_natureOCT"}
+RESUME=${3:-""}
+INPUT_SIZE=${4:-224}
+SMPMode=${5:-"dec"} # dec, enc, fuse
+SMPFuseMode=${6:-"weighted_sum"} # ("weighted_sum", "add", "channel_merge", "channel_multiply", "multiply")
+SMPAlpha=${7:-0.5} # 0.0-1.0
+SMPSizeMatch=${8:-"decoder_to_encoder"} # decoder_to_encoder, encoder_to_decoder
+FUSION_DIM=${9:-0} # 0 for default
+ALIGN=${10:-"pre"} # 0 for default
+ENC_IDX=${11:-"-1"} # -1 for last encoder layer
+DEC_IDX=${12:-"-1"} # -1 for last decoder layer
+TARGET_MODULE=${13:-"encoder"} # encoder, decoder, head
+SELECT_INDEX=${14:-"-1"} # -1 for last layer
+SMPClassifier=${15:-"linear"} # linear, conv
+ADDCMD=${16:-""}
+ADDCMD2=${17:-""}
+ADDCMD3=${18:-""}
 
-SCRIPT=$1
-# Classifier checkpoint to load (main-style --resume)
-RESUME_CKPT=$2
-# SMP segmentation checkpoint to load into SMPClassifier (main-style --finetune)
-SEG_CKPT=${3:-"/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth"}
-
-# SMP hyper-params (follow finetune_retfound_UFbenchmark_irb2024v5_smp.sh conventions)
-SMPMode=${4:-"dec"}                 # dec, enc, fuse
-SMPFuseMode=${5:-"weighted_sum"}    # weighted_sum/add/channel_merge/channel_multiply/multiply
-SMPAlpha=${6:-0.5}                  # 0.0-1.0
-SMPSizeMatch=${7:-"decoder_to_encoder"}  # decoder_to_encoder / encoder_to_decoder
-FUSION_DIM=${8:-0}
-ALIGN=${9:-"pre"}                   # pre / post
-ENC_IDX=${10:-"-1"}
-DEC_IDX=${11:-"-1"}
-SMPClassifier=${12:-"linear"}       # linear / conv
-
-OUTPUT_DIR=${13:-"./heatmap_debug_batch"}
-batch_size=${14:-4}
+OUTPUT_DIR="./heatmap_params_tuning"
 
 # ============================================================================
 # Common parameters
@@ -47,7 +49,15 @@ DATASET_DIR="/blue/ruogu.fang/tienyuchang/OCT_EDA"
 DATASET_FNAME="sampled_labels01.csv"
 THICKNESS_DIR="/orange/ruogu.fang/tienyuchang/IRB2024_OCT_thickness/Data/"
 THICKNESS_CSV="/orange/ruogu.fang/tienyuchang/IRB2024_OCT_thickness/thickness_map.csv"
-target_module="encoder decoder head"
+BATCH_SIZE=8
+NUM_SAMPLES=50
+Num_CLASS=2
+STEP_PIXELS=1024
+#XAI_METHODS="gradcamv2 scorecam crp"
+#XAI_METHODS="gradcamv2 hirescam gradcam++"
+XAI_METHODS="hirescam"
+
+#sbatch run_xai_layermap_bs.sh SMP /blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth /orange/ruogu.fang/tienyuchang/RETfound_results/DME_binary_all_split-IRB2024_v5-all-/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass.pth-OCT-bs16ep100lr5e-4optadamw-defaulteval-trsub0-enc-smpweighted_sum-\{0\}-fea-1-1-0.5-decoder_to_encoder-conv---/checkpoint-best.pth 512 enc weighted_sum 0.5 decoder_to_encoder 0 pre -1 -1 encoder -1 conv
 
 # ============================================================================
 # Run XAI for base models (DME_finetuned)
@@ -57,9 +67,9 @@ python "$SCRIPT" \
         --dataset_fname "$DATASET_FNAME" \
         --thickness_dir "$THICKNESS_DIR" \
         --thickness_csv "$THICKNESS_CSV" \
-        --model "SMP" \
-        --finetune "$SEG_CKPT" \
-        --resume "$RESUME_CKPT" \
+        --model "$MODEL" \
+        --finetune "$FINETUNED_MODEL" \
+        --resume "$RESUME" \
         --SMPMode "$SMPMode" \
         --smp_fuse_mode "$SMPFuseMode" \
         --smp_alpha "$SMPAlpha" \
@@ -69,14 +79,15 @@ python "$SCRIPT" \
         --enc_idx "$ENC_IDX" \
         --dec_idx "$DEC_IDX" \
         --smp_classifier "$SMPClassifier" \
-        --target_module "$target_module" \
-        --task DME \
-        --num_samples -1 \
-        --xai "gradcam hirescam gradcam++" \
-        --batch_size $batch_size \
-        --input_size 512 \
-        --nb_classes 2 \
+        --target_module "$TARGET_MODULE" \
+        --select_index "$SELECT_INDEX" \
+        --task "$STUDY" \
+        --num_samples $NUM_SAMPLES \
+        --xai "$XAI_METHODS" \
+        --batch_size $BATCH_SIZE \
+        --input_size $INPUT_SIZE \
+        --nb_classes "$Num_CLASS" \
         --load_mask \
         --draw_layer \
         --output_dir "$OUTPUT_DIR" \
-        --verbose
+        $ADDCMD $ADDCMD2 $ADDCMD3
