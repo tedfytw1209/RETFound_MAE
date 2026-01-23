6c2742ebc5b33c79473d716d7ead7cbcf8bb4104
teddy <teddytw1209@gmail.com>
2026-01-23 09:51:00 -0500

    eval smp full

--- finetune_retfound_UFbenchmark_v5_eval_full.sh
+++ finetune_retfound_UFbenchmark_v5_eval_full.sh
@@ -22,7 +22,7 @@ FINETUNED_MODEL=${3:-"RETFound_mae_natureOCT"}
 RESUME=${4:-"0"} # resume path
 Num_CLASS=${5:-"2"} # 2 for AMD, 5 for DR, 5 for Glaucoma, 2 for Cataract
 INPUT_SIZE=${6:-"224"}
-XAI=${7:-"attn"} # attn, rise, gradcam
+XAI=${7:-"attn"} # XAI_METHODS=("hirescam" "gradcam++" "gradcamv2" "crp")  # List of XAI methods
 STEP_PIXELS=${8:-"224"}
 ADD_WORDS=${9:-""} # additional words to specify the finetuned model
 ADD_WORDS2=${10:-""} # additional words to specify the finetuned model
@@ -37,5 +37,5 @@ echo $Num_CLASS
 
 # Modify the path to your singularity container 
 # sbatch finetune_retfound_UFbenchmark_v5_eval_full.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT /orange/ruogu.fang/tienyuchang/RETfound_results/DME_binary_all_split-IRB2024_v5-all-RETFound_mae_natureOCT-OCT-bs16ep50lr5e-4optadamw-defaulteval-trsub0---add_mask---train_no_aug/checkpoint-best.pth 2 224 attn
-#sbatch finetune_retfound_UFbenchmark_v5_eval_full.sh AMD_all_split RETFound_mae RETFound_mae_natureOCT /orange/ruogu.fang/tienyuchang/RETfound_results/DR_binary_all_split-IRB2024_v5-all-RETFound_mae_natureOCT-OCT-bs16ep50lr5e-4optadamw-defaulteval-trsub0--/checkpoint-best.pth 2 224 hirescam
+#sbatch finetune_retfound_UFbenchmark_v5_eval_full.sh DME_binary_all_split RETFound_mae RETFound_mae_natureOCT /orange/ruogu.fang/tienyuchang/RETfound_results/DR_binary_all_split-IRB2024_v5-all-RETFound_mae_natureOCT-OCT-bs16ep50lr5e-4optadamw-defaulteval-trsub0--/checkpoint-best.pth 2 224 hirescam
 TIMM_FUSED_ATTN=0 python main_XAI_evaluation.py --batch_size 4     --model $MODEL     --nb_classes $Num_CLASS     --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/${data_type}/split/tune8-eval2/${STUDY}.csv     --task $STUDY-$data_type-$FINETUNED_MODEL-$ADD_WORDS-XAI${XAI}-EVAL/ --img_dir /orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/ --finetune $FINETUNED_MODEL --num_workers 8 --input_size $INPUT_SIZE --num_k $NUM_K --resume $RESUME --xai $XAI --step_pixels $STEP_PIXELS --output_mask $ADD_WORDS $ADD_WORDS2
