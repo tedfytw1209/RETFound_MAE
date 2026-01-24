c74e173a1c3dad23234d95f3b9f6ba743f4a2499
teddy <teddytw1209@gmail.com>
2026-01-24 14:16:22 -0500

    Update run_xai_layermap_multirun.sh

--- run_xai_layermap_multirun.sh
+++ run_xai_layermap_multirun.sh
@@ -19,6 +19,8 @@ conda activate octxai
 STUDY=${1:-"DME"}
 STUDY_NAME=${2:-"DME_binary_all_split"}
 
+# sbatch run_xai_layermap_bs.sh DME DME_binary_all_split
+# ============================================================================
 # Baselines
 bash run_xai_layermap_bs.sh $STUDY RETFound_mae RETFound_mae_natureOCT /orange/ruogu.fang/tienyuchang/RETfound_results/${STUDY_NAME}-IRB2024_v5_all-all-RETFound_mae_natureOCT-OCT-bs16ep50lr5e-4optadamw-defaulteval-trsub0--/checkpoint-best.pth 224 dec weighted_sum 0.5 decoder_to_encoder 0 pre -1 -1 encoder -1 conv
 bash run_xai_layermap_bs.sh  $STUDY vit-base-patch16-224 google/vit-base-patch16-224-in21k /orange/ruogu.fang/tienyuchang/RETfound_results/${STUDY_NAME}-IRB2024_v5_all-all-google/vit-base-patch16-224-in21k-OCT-bs16ep50lr5e-4optadamw-defaulteval-trsub0--/checkpoint-best.pth 224 dec weighted_sum 0.5 decoder_to_encoder 0 pre -1 -1 encoder -1 conv
