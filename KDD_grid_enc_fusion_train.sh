84cdf42939079a5f12d2b4b0770a6a5f339b0525
teddy <teddytw1209@gmail.com>
2026-04-08 14:55:00 -0400

    Update KDD_grid_enc_fusion_train.sh

--- KDD_grid_enc_fusion_train.sh
+++ KDD_grid_enc_fusion_train.sh
@@ -11,12 +11,14 @@
 
 BASE_CKPT=/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_multiclass_resnet50_new.pth
 
-ENC_IDXS=(-1 -2 -3)
-FUSION_DIMS=(4 9 16 32)
+ENC_IDXS=(-2)
+#ENC_IDXS=(-1 -2 -3)
+#FUSION_DIMS=(4 9 16 32)
+FUSION_DIMS=(4 16 32)
 
 for ENC_IDX in "${ENC_IDXS[@]}"; do
     for FUSION_DIM in "${FUSION_DIMS[@]}"; do
-        echo "Submitting ENC_IDX=${ENC_IDX}, FUSION_DIM=${FUSION_DIM} ..."
+        #echo "Submitting ENC_IDX=${ENC_IDX}, FUSION_DIM=${FUSION_DIM} ..."
         sbatch finetune_retfound_UFbenchmark_irb2024v5_smp_full.sh \
             DME_binary_all_split \
             SMP \
