219dded656268e7c9528d9625c8ce776f8671d76
teddy <teddytw1209@gmail.com>
2026-04-14 00:09:06 -0400

    Update KDD_grid_enc_fusion_train.sh

--- KDD_grid_enc_fusion_train.sh
+++ KDD_grid_enc_fusion_train.sh
@@ -14,7 +14,7 @@ BASE_CKPT=/blue/ruogu.fang/tienyuchang/RETFound_MAE/Seg_checkpoints/best_model_m
 ENC_IDXS=(-1 -3)
 #ENC_IDXS=(-1 -2 -3)
 #FUSION_DIMS=(4 9 16)
-FUSION_DIMS=(4 9 16)
+FUSION_DIMS=(9)
 
 for ENC_IDX in "${ENC_IDXS[@]}"; do
     for FUSION_DIM in "${FUSION_DIMS[@]}"; do
