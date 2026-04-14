0b9fd9e77f8bdf0ad49fef3e342683ff688d384f
teddy <teddytw1209@gmail.com>
2026-04-14 09:21:20 -0400

    Update KDD_grid_enc_fusion_train.sh

--- KDD_grid_enc_fusion_train.sh
+++ KDD_grid_enc_fusion_train.sh
@@ -19,7 +19,7 @@ FUSION_DIMS=(4 16 32)
 for ENC_IDX in "${ENC_IDXS[@]}"; do
     for FUSION_DIM in "${FUSION_DIMS[@]}"; do
         echo "Submitting ENC_IDX=${ENC_IDX}, FUSION_DIM=${FUSION_DIM} ..."
-        sbatch finetune_retfound_UFbenchmark_v5_smp_full.sh \
+        sbatch finetune_retfound_UFbenchmark_irb2024v5_smp_full.sh \
             DME_binary_all_split \
             SMP \
             ${BASE_CKPT} \
@@ -45,7 +45,7 @@ for ENC_IDX in "${ENC_IDXS[@]}"; do
     done
 done
 
-ENC_IDXS=(-1)
+ENC_IDXS=(-1 -3)
 #ENC_IDXS=(-1 -2 -3)
 #FUSION_DIMS=(4 9 16)
 FUSION_DIMS=(9)
@@ -53,7 +53,7 @@ FUSION_DIMS=(9)
 for ENC_IDX in "${ENC_IDXS[@]}"; do
     for FUSION_DIM in "${FUSION_DIMS[@]}"; do
         echo "Submitting ENC_IDX=${ENC_IDX}, FUSION_DIM=${FUSION_DIM} ..."
-        sbatch finetune_retfound_UFbenchmark_v5_smp_full.sh \
+        sbatch finetune_retfound_UFbenchmark_irb2024v5_smp_full.sh \
             DME_binary_all_split \
             SMP \
             ${BASE_CKPT} \
